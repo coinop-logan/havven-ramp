9c08a869ddd654ce43608cb91a7d8e4debbae9da
coinop-logan <coinop.logan@gmail.com>
2018-05-01 01:02:47 +0000

    Begin work on ApprovalBP.sol

---
+++ ApprovalBP.sol
@@ -0,0 +1,231 @@
+// An ApprovalBP is a modification of the BurnablePayment contract.
+// This replaces the worker-called "Commit" function with the payer-called "ApproveWorker" function.
+// This allows a user without ETH to be registered as the recipient,
+// given that they have an independent method of contacting the payer and requesting such an approval.
+//
+// This modification has been sponsored by Havven,
+// to be used in the development of a robust, decentralized crypto on-ramp.
+
+pragma solidity ^ 0.4.23;
+
+contract ApprovalBPFactory {
+    event NewApprovalBP(
+        address indexed bpAddress,
+        address payer,
+        uint deposited,
+        uint autoreleaseInterval,
+        string title,
+        string initialStatement
+    );
+
+    function newBP(address payer, uint autoreleaseInterval, string title, string initialStatement)
+    external
+    payable
+    returns (address)
+    {
+        //pass along any ether to the constructor
+        address newBPAddr = (new ApprovalBP).value(msg.value)(payer, autoreleaseInterval, title, initialStatement);
+        emit NewApprovalBP(newBPAddr, payer, msg.value, autoreleaseInterval, title, initialStatement);
+
+        return newBPAddr;
+    }
+}
+
+contract ApprovalBP {
+    //title will never change
+    string public title;
+
+    //BP will start with a payer but not a recipient
+    address public payer;
+    address public recipient;
+
+    address constant BURN_ADDRESS = 0x0;
+
+    //Set to true if fundsRecovered is called (offers another method of detecting recalled BPs)
+    bool recovered = false;
+
+    //Note that these will track, but not influence the BP logic.
+    uint public amountDeposited;
+    uint public amountBurned;
+    uint public amountReleased;
+
+    //How long should we wait before allowing the default release to be called?
+    uint public autoreleaseInterval;
+
+    //Calculated from autoreleaseInterval in commit(),
+    //and recaluclated whenever the payer calls delayDefaultRelease()
+    //After this time, auto-release can be called by any address.
+    uint public autoreleaseTime;
+
+    //Most action happens in the Committed state.
+    enum State {
+        Open,
+        Committed,
+        Closed
+    }
+
+    //Note that a BP cannot go from Committed back to either Open state, but it can go from Closed back to Committed
+    //Search for Closed and Unclosed events to see how this works.
+    State public state;
+
+    modifier inState(State s) {
+        require(s == state);
+        _;
+    }
+    modifier onlyPayer() {
+        require(msg.sender == payer);
+        _;
+    }
+
+    event Created(address indexed contractAddress, address payer, uint autoreleaseInterval, string title);
+    event FundsAdded(uint amount); //The payer has added funds to the BP.
+    event PayerStatement(string statement);
+    event FundsRecovered();
+    event RecipientApproved(address recipient);
+    event FundsBurned(uint amount);
+    event FundsReleased(uint amount);
+    event Closed();
+    event Unclosed();
+    event AutoreleaseDelayed();
+    event AutoreleaseTriggered();
+
+    constructor(address _payer, uint _autoreleaseInterval, string _title, string initialStatement)
+    public
+    payable
+    {
+        emit Created(this, payer, autoreleaseInterval, title);
+
+        payer = _payer;
+        title = _title;
+        state = State.Open;
+        autoreleaseInterval = _autoreleaseInterval;
+
+        if (msg.value > 0) {
+            emit FundsAdded(msg.value);
+            amountDeposited += msg.value;
+        }
+
+        if (bytes(initialStatement).length > 0) {
+            emit PayerStatement(initialStatement);
+        }
+    }
+
+    function addFunds()
+    external
+    payable
+    onlyPayer()
+    {
+        require(msg.value > 0);
+
+        emit FundsAdded(msg.value);
+
+        amountDeposited += msg.value;
+        if (state == State.Closed) {
+            state = State.Committed;
+            emit Unclosed();
+        }
+    }
+
+    function recoverFunds()
+    external
+    onlyPayer()
+    inState(State.Open)
+    {
+        recovered = true;
+        emit FundsRecovered();
+
+        selfdestruct(payer);
+    }
+
+    function approveRecipient(address _recipient)
+    external
+    payable
+    onlyPayer()
+    inState(State.Open)
+    {
+        recipient = _recipient;
+
+        state = State.Committed;
+
+        emit RecipientApproved(recipient);
+
+        autoreleaseTime = now + autoreleaseInterval;
+    }
+
+    function internalBurn(uint amount)
+    internal
+    {
+        BURN_ADDRESS.transfer(amount);
+
+        amountBurned += amount;
+        emit FundsBurned(amount);
+
+        if (address(this).balance == 0) {
+            state = State.Closed;
+            emit Closed();
+        }
+    }
+
+    function burn(uint amount)
+    external
+    inState(State.Committed)
+    onlyPayer()
+    {
+        internalBurn(amount);
+    }
+
+    function internalRelease(uint amount)
+    internal
+    {
+        recipient.transfer(amount);
+
+        amountReleased += amount;
+        emit FundsReleased(amount);
+
+        if (address(this).balance == 0) {
+            state = State.Closed;
+            emit Closed();
+        }
+    }
+
+    function release(uint amount)
+    external
+    inState(State.Committed)
+    onlyPayer()
+    {
+        internalRelease(amount);
+    }
+
+    function logPayerStatement(string statement)
+    external
+    onlyPayer()
+    {
+        emit PayerStatement(statement);
+    }
+
+    function delayAutorelease()
+    external
+    onlyPayer()
+    inState(State.Committed)
+    {
+        autoreleaseTime = now + autoreleaseInterval;
+        emit AutoreleaseDelayed();
+    }
+
+    function triggerAutorelease()
+    external
+    inState(State.Committed)
+    {
+        require(now >= autoreleaseTime);
+
+        emit AutoreleaseTriggered();
+        internalRelease(address(this).balance);
+    }
+
+    function getFullState()
+    external
+    constant
+    returns(State, address, address, string, uint, uint, uint, uint, uint, uint) {
+        return (state, payer, recipient, title, address(this).balance, amountDeposited, amountBurned, amountReleased, autoreleaseInterval, autoreleaseTime);
+    }
+}
